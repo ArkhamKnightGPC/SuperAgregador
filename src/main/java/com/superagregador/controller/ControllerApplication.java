6047645cd1eaf5866fe1a226e589a182709dee32
Eduardo Santos <santos.eduardothomaz@gmail.com>
2020-05-22 12:20:25 -0300

    Criei o frontend do site

--- src/main/java/com/superagregador/controller/ControllerApplication.java
+++ src/main/java/com/superagregador/controller/ControllerApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = { "com.superagregador"})
 @Controller
 public class ControllerApplication {
 	public static void main(String[] args) {
@@ -15,9 +16,20 @@ public class ControllerApplication {
 	}
 
 	@GetMapping("/")
-	public String ola(@RequestParam(value = "nome", required=false, defaultValue = "Usuário Desconhecido") String nome,
+	public String home(@RequestParam(value = "nome", required=false, defaultValue = "Usuário Desconhecido") String nome,
 	Model model) {
 		model.addAttribute("nome", nome);
+		return "index";
+	}
+
+	@GetMapping("/sobre")
+	public String sobre(Model model) {
+		return "sobre";
+	}
+
+	@PostMapping("/AdicionarSite")
+	public String adicionarSite(@RequestParam(value = "url", required = true, defaultValue = "") String URL, Model model) {
+		model.addAttribute("valorURL", URL);
 		return "index";
 	}
 }
