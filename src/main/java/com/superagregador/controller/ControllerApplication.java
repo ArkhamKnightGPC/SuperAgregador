16d3ceed1eb5c877aab77da725de73769eed6db5
Eduardo <santos.eduardothomaz@gmail.com>
2020-05-22 20:10:56 -0300

    Torna possivel remover e adicionar fontes

--- src/main/java/com/superagregador/controller/ControllerApplication.java
+++ src/main/java/com/superagregador/controller/ControllerApplication.java
@@ -8,17 +8,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
+import com.superagregador.models.Blog;
+import com.superagregador.models.EditarBlog;
+
 @SpringBootApplication (scanBasePackages = { "com.superagregador"})
 @Controller
 public class ControllerApplication {
+	private static EditarBlog blogs;
+	private static String nomePadrao = "";
+	private static String uriPadrao = "";
+	private static boolean existemBlogs;
 	public static void main(String[] args) {
 		SpringApplication.run(ControllerApplication.class, args);
+		blogs = EditarBlog.inicializador();
 	}
 
 	@GetMapping("/")
-	public String home(@RequestParam(value = "nome", required=false, defaultValue = "Usuário Desconhecido") String nome,
-	Model model) {
-		model.addAttribute("nome", nome);
+	public String home(Model model) {
+		if (blogs.getMap().isEmpty()) {
+			existemBlogs = false;
+		} else existemBlogs = true;
+
+		model.addAttribute("blogs", blogs.getMap());
+		model.addAttribute("existemBlogs", existemBlogs);
+		model.addAttribute("valorNome", nomePadrao);
+		model.addAttribute("valorURI", uriPadrao);
 		return "index";
 	}
 
@@ -28,8 +42,26 @@ public class ControllerApplication {
 	}
 
 	@PostMapping("/AdicionarSite")
-	public String adicionarSite(@RequestParam(value = "url", required = true, defaultValue = "") String URL, Model model) {
-		model.addAttribute("valorURL", URL);
-		return "index";
+	public String adicionarSite(@RequestParam(value = "nome", required = true, defaultValue = "") String nome,
+								@RequestParam(value = "uri", required = true, defaultValue = "") String uri,
+								Model model) {
+							
+		if (!nome.equals("") && !uri.equals("")) {
+			blogs.adicionarBlog(new Blog (EditarBlog.getMaxID(), uri, nome));
+			nomePadrao = "";
+			uriPadrao = "";
+		} else {
+			nomePadrao = nome;
+			uriPadrao = uri;
+		}
+
+		return "redirect:/";
 	}
+
+	@PostMapping("/RemoverSite")
+	public String removerSite(@RequestParam(value= "id", required = true) int id) {
+		blogs.removerBlog((Integer) id);
+		return "redirect:/";
+	}
+
 }
---
+++ src/main/java/com/superagregador/models/Blog.java
@@ -0,0 +1,43 @@
+package com.superagregador.models;
+
+public class Blog {
+	
+	private int ID;
+    public String uri;
+    public String nome;
+	//private String user;
+    
+    public Blog(int ID, String uri, String nome) {
+        this.ID = ID;
+        this.uri = uri;
+        this.nome = nome;
+    }
+    
+	public String getUri() {
+		return uri;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public int ID() {
+        return ID;
+    }
+
+	public void setUri(String uri) {
+		this.uri = uri;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    
+
+
+}
